b42395386a6e9d22dffae839c26d6438c68c67d7
Joanna Jacob <joannajacob910@gmail.com>
2025-12-18 00:29:53 +0530

    completed 1.5

--- src/App.jsx
+++ src/App.jsx
@@ -22,29 +22,32 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
 
   return (
     <div>
       <strong>
-        <Header course = {course}/>
+        <Header course = {course.name}/>
       </strong>
-      <Content part={parts}/>
-      <Total part={parts}/>
+      <Content part={course.parts}/>
+      <Total part={course.parts}/>
     </div>
   )
 }
